dcb4f0fff6648f24f1a3e4750573b89f9a60133b
Dylan Bomgardner <dylan.m.bomgardner@gmail.com>
2025-11-12 13:56:04 -0700

    added an sdk build target

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Author: Dylan Bomgardner
 set -e
-set -x
 
+BUILD_SDK="$1"
 BR_DIR="$(pwd)/buildroot"
 BR2_EXTERNAL="$(pwd)/fsae_dash"
 DEFCONFIG_NAME="fsae_dash_defconfig"
@@ -10,19 +10,26 @@ OUTPUT_DIR="$(pwd)/buildroot"
 BUILD_DIR="$(pwd)/build"
 
 # Create the build dir
-mkdir -p "BUILD_DIR"
+mkdir -p "$BUILD_DIR"
 
 
-echo "Running menuconfig"
+echo "Building"
 pushd $BR_DIR > /dev/null
 # Run the defconfig pointing to our config
 make BR2_EXTERNAL="$BR2_EXTERNAL" $DEFCONFIG_NAME
 
 # Build command
-make
+if [ "${BUILD_SDK}" = "sdk" ]; then
+    echo "Building SDK"
+    make -j$(nproc) sdk
+else
+    make -j$(nproc)
+    cp "$BUILDROOT_DIR/output/images/"*.img "$CUSTOM_OUTPUT_DIR" || true
+    cp "$BUILDROOT_DIR/output/images/"*.tar.gz "$CUSTOM_OUTPUT_DIR" || true
+fi
+
 popd > /dev/null
 
-cp "$BUILDROOT_DIR/output/images/"*.img "$CUSTOM_OUTPUT_DIR" || true
-cp "$BUILDROOT_DIR/output/images/"*.tar.gz "$CUSTOM_OUTPUT_DIR" || true
+
 
 echo -e "\e[32mBuild Succeeded.\e[0m"
